fa05634cdccd664cf57c35de7425a3579a3b2d1a
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2022-12-18 00:49:23 +0630

    update

---
+++ components/Content.vue
@@ -0,0 +1,302 @@
+<template>
+  <div class="div_container">
+    <!-- Student Plan -->
+    <v-row class="student_plan_container">
+      <v-col cols="12" md="6" sm="6" class="student_plan_left">
+        <h3>
+          The Apple Music <br />
+          Student Plan comes <br />
+          with Apple TV+ for free.
+        </h3>
+        <div class="try_free">Try Apple Music free</div>
+      </v-col>
+      <v-col cols="12" md="6" sm="6" class="student_plan_right">
+        <img
+          src="https://www.apple.com/v/apple-tv-plus/ag/images/overview/student_plan__bm7yp0v4tf6u_large.jpg"
+        />
+      </v-col>
+    </v-row>
+
+    <!-- TV4K & AirPlay -->
+    <div class="lower_container">
+      <!-- TV 4K -->
+      <div class="tv_4k">
+        <!-- New -->
+        <div class="violator-frameless">New</div>
+        <img
+          class="apple_tv_4k"
+          src="https://www.apple.com/v/apple-tv-plus/ag/images/overview/apple_tv_4k_logo__bx1mtrage32a_large.png"
+          alt="appletv4k"
+        />
+
+        <!-- Headline -->
+        <div class="headline">
+          The Apple experience. <br />
+          Cinematic in every sense.
+        </div>
+
+        <!-- Typography -->
+        <div class="typography_container">
+          <div class="button">Buy</div>
+          <div class="learn_more">Learn more</div>
+          <img
+            src="https://img.icons8.com/material-outlined/18/0f64ba/forward.png"
+          />
+        </div>
+
+        <!-- Image -->
+        <img
+          class="tv4k_img"
+          src="https://www.apple.com/v/apple-tv-plus/ag/images/overview/apple_tv_4k_remote__igmcbznr1w2u_large.jpg"
+          alt="tv4k"
+        />
+      </div>
+
+      <!-- AirPlay -->
+      <div class="airplay">
+        <!-- Eyebrow -->
+        <div class="eyebrow">AirPlay</div>
+
+        <!-- Headline -->
+        <div class="headline">
+          Bring Apple TV+ to <br />
+          a screen near you.
+        </div>
+
+        <!-- Learn More -->
+        <div class="learn_more_container">
+          <div class="learn_more">Learn more</div>
+          <img
+            src="https://img.icons8.com/material-outlined/18/0f64ba/forward.png"
+          />
+        </div>
+
+        <!-- Image -->
+        <img
+          class="airplay_img"
+          src="https://www.apple.com/v/apple-tv-plus/ag/images/overview/air_play__dyuvvwnd5riq_large.jpg"
+          alt="airplay"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.div_container {
+  width: 97%;
+  margin: 0 auto;
+  background-color: #fff;
+  //   Student Plan
+  .student_plan_container {
+    max-width: 1360px;
+    align-items: flex-end;
+    height: 530px;
+    margin: 0 auto;
+    background-color: #f5f5f7;
+    .student_plan_left {
+      max-width: 535px;
+      margin-left: auto;
+      height: 450px;
+      h3 {
+        font-size: clamp(30px, 4vw, 48px);
+        line-height: 1.0834933333;
+        font-weight: 700;
+        letter-spacing: -0.003em;
+        margin: 18px 0 1px;
+        color: #1d1d1f;
+      }
+      .try_free {
+        margin-top: 13px;
+        display: inline-block;
+        text-align: center;
+        min-width: 28px;
+        border-radius: 980px;
+        background: #0071e3;
+        font-size: 17px;
+        line-height: 1.1764805882;
+        letter-spacing: -0.022em;
+        min-width: 26px;
+        padding: 12px 22px;
+        font-size: 14px;
+        line-height: 1.4285914286;
+        font-weight: 400;
+        letter-spacing: -0.016em;
+        font-weight: 700;
+        background-color: #fff;
+        color: #1d1d1f;
+        cursor: pointer;
+        color: #fff;
+        background-color: #1d1d1f;
+      }
+    }
+    .student_plan_right {
+      background-size: contain;
+      max-height: 450px;
+      padding: 0px !important;
+      img {
+        max-width: 533px;
+        width: 100%;
+        height: auto;
+      }
+    }
+    @media only screen and (max-width: 1000px) {
+      .student_plan_left {
+        height: 300px;
+      }
+    }
+    @media only screen and (max-width: 599px) {
+      .student_plan_left {
+        height: 220px;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        h3 {
+          text-align: center;
+        }
+        .try_free {
+          margin: 20px auto;
+        }
+      }
+      .student_plan_right {
+        display: flex;
+        img {
+          margin: 0 auto;
+          width: 60%;
+          height: auto;
+        }
+      }
+    }
+  }
+
+  //  TV4K & AirPlay
+  .lower_container {
+    max-width: 1360px;
+    margin: 40px auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    // TV4K
+    .tv_4k {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 48.5%;
+      height: 100%;
+      background-color: #f5f5f7;
+      .violator-frameless {
+        color: #bf4800;
+        font-size: 17px;
+        margin: 80px 0 10px;
+      }
+      .apple_tv_4k {
+        width: 116px;
+        height: 35px;
+      }
+      .headline {
+        font-size: clamp(30px, 4vw, 48px);
+        line-height: 1.0834933333;
+        font-weight: 700;
+        letter-spacing: -0.003em;
+        color: #1d1d1f;
+        text-align: center;
+        margin-top: 21px;
+        margin-bottom: 17px;
+      }
+      .typography_container {
+        display: flex;
+        align-items: center;
+        padding-bottom: 100px;
+        .button {
+          margin: 0 0.8em;
+          font-size: 17px;
+          line-height: 1.1764805882;
+          font-weight: 400;
+          letter-spacing: -0.022em;
+          min-width: 26px;
+          padding: 12px 22px;
+          border-radius: 980px;
+          background: #0071e3;
+          color: #fff;
+        }
+        .learn_more {
+          color: #06c;
+          font-size: 19px;
+          line-height: 1.2105263158;
+          font-weight: 400;
+        }
+      }
+      .tv4k_img {
+        width: 100%;
+      }
+    }
+    // AirPlay
+    .airplay {
+      text-align: center;
+      width: 48.5%;
+      background-color: #f5f5f7;
+      .eyebrow {
+        margin-top: 110px;
+        font-size: 32px;
+        line-height: 1.125;
+        font-weight: 600;
+        letter-spacing: 0.004em;
+      }
+      .headline {
+        font-size: clamp(30px, 4vw, 48px);
+        line-height: 1.0834933333;
+        font-weight: 700;
+        letter-spacing: -0.003em;
+        margin-top: 15px;
+        margin-bottom: 36px;
+      }
+      .learn_more_container {
+        display: flex;
+        align-items: center;
+        width: 110px;
+        margin: 0 auto 100px;
+        .learn_more {
+          color: #06c;
+          font-size: 19px;
+          line-height: 1.2105263158;
+          font-weight: 400;
+        }
+      }
+      .airplay_img {
+        width: 100%;
+      }
+    }
+  }
+
+  @media (min-width: 750px) and (max-width: 1000px) {
+    .student_plan_container {
+      height: 430px;
+    }
+  }
+  @media (min-width: 599px) and (max-width: 750px) {
+    .student_plan_container {
+      height: 330px;
+    }
+  }
+  @media (max-width: 750px) {
+    .tv_4k {
+      width: 100% !important;
+      .typography_container {
+        padding-bottom: 20px !important;
+      }
+      .tv4k_img {
+        width: 80% !important;
+      }
+    }
+    .airplay {
+      width: 100% !important;
+      .learn_more_container {
+        margin: 0 auto 20px !important;
+      }
+      .airplay_img {
+        width: 80% !important;
+      }
+    }
+  }
+}
+</style>
---
+++ components/Footer.vue
@@ -0,0 +1,318 @@
+<template>
+  <div class="div_container">
+    <!-- Foot Notes -->
+    <div class="foot_notes_container">
+      <div>
+        <div class="foot_notes">* MLS Season Pass requires a subscription.</div>
+        <div class="foot_notes">
+          1. $6.99/month after free trial. Only one offer per Apple ID and only
+          one offer per family if you’re part of a Family Sharing group,
+          regardless of the number of devices you or your family purchases. This
+          offer is not available if you or your Family have previously accepted
+          an Apple TV+ one year free offer. Offer good for 3 months after
+          eligible device activation. Plan automatically renews until cancelled.
+          Restrictions and other terms apply.
+        </div>
+        <div class="foot_notes">
+          2. One subscription per Family Sharing group. Plan automatically
+          renews until cancelled.
+        </div>
+        <div class="foot_notes">
+          3. The Apple One free trial includes only services that you are not
+          currently using through a free trial or a subscription. Plan
+          automatically renews after trial until cancelled. Restrictions and
+          other terms apply.
+        </div>
+        <div class="foot_notes">
+          4. Limited-time offer; offer may end at any time. Free Apple TV+
+          access for students ends when you no longer qualify or do not renew
+          your Apple Music Student subscription. Offer good for verified college
+          students only and does not extend to a Family Sharing group.
+        </div>
+      </div>
+    </div>
+
+    <!-- Breadcrumbs -->
+    <div class="breadcrumbs_container">
+      <div class="breadcrumbs">
+        <div>
+          <img src="https://img.icons8.com/ios-glyphs/16/null/mac-os.png" />
+        </div>
+        <div>
+          <img
+            src="https://img.icons8.com/material-sharp/15/424245/chevron-right.png"
+          />
+        </div>
+        <div>Only on Apple</div>
+        <div>
+          <img
+            src="https://img.icons8.com/material-sharp/15/424245/chevron-right.png"
+          />
+        </div>
+        <div>Apple TV+</div>
+      </div>
+    </div>
+
+    <!-- Apple Directory -->
+    <div class="apple_directory_container" v-show="!$vuetify.breakpoint.xs">
+      <v-row>
+        <v-col cols="12" lg="2" md="3" sm="4">
+          <div class="apple_title">Shop and Learn</div>
+          <div>Store</div>
+          <div>Mac</div>
+          <div>iPad</div>
+          <div>iPhone</div>
+          <div>Watch</div>
+          <div>AirPods</div>
+          <div>TV & Home</div>
+          <div>AirTag</div>
+          <div>Accessories</div>
+          <div>Gift Cards</div>
+        </v-col>
+        <v-col cols="12" lg="2" md="3" sm="4">
+          <div class="apple_title">Services</div>
+          <div>Apple Music</div>
+          <div>Apple TV+</div>
+          <div>Apple Fitness+</div>
+          <div>Apple News+</div>
+          <div>Apple Arcade</div>
+          <div>iCloud</div>
+          <div>Apple One</div>
+          <div>Apple Card</div>
+          <div>Apple Books</div>
+          <div>Apple Podcasts</div>
+          <div>App Store</div>
+          <div class="apple_title" style="padding-top: 24px">Account</div>
+          <div>Manage Your Apple ID</div>
+          <div>Apple Store Account</div>
+          <div>iCloud.com</div>
+        </v-col>
+        <v-col cols="12" lg="2" md="3" sm="4">
+          <div class="apple_title">Apple Store</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+        </v-col>
+        <v-col cols="12" lg="2" md="3" sm="4">
+          <div class="apple_title">For Business</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div class="apple_title" style="padding-top: 24px">For Education</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div class="apple_title" style="padding-top: 24px">
+            For Healthcare
+          </div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div class="apple_title" style="padding-top: 24px">
+            For Government
+          </div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+        </v-col>
+        <v-col cols="12" lg="2" md="3" sm="4">
+          <div class="apple_title">Apple Values</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div class="apple_title" style="padding-top: 24px">About Apple</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+          <div>xaxax</div>
+        </v-col>
+      </v-row>
+    </div>
+
+    <div class="apple_directory_container" v-show="$vuetify.breakpoint.xs">
+      <div class="section_title" v-for="(item, i) in apple_items" :key="i">
+        <span>{{ item.title }}</span>
+        <img
+          src="https://img.icons8.com/external-tanah-basah-detailed-outline-tanah-basah/15/424245/external-plus-user-interface-tanah-basah-detailed-outline-tanah-basah-2.png"
+        />
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="footer_container">
+      <div class="footer_shop">
+        More ways to shop:
+        <span class="link">Find an Apple Store</span> or
+        <span class="link"> other retailer</span> near you. Or call
+        1-800-MY-APPLE.
+      </div>
+      <div class="footer_legal">
+        <div class="legal_link">
+          Copyright © 2022 Apple Inc. All rights reserved.
+        </div>
+        <div class="copyright border_r">Privacy Policy</div>
+        <div class="legal_link border_r">Terms of Use</div>
+        <div class="legal_link border_r">Sales and Refunds</div>
+        <div class="legal_link border_r">Legal</div>
+        <div class="legal_link">Site Map</div>
+        <v-spacer />
+        <div>United States</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    apple_items: [
+      {
+        title: 'Shop and Learn',
+        isShow: false,
+      },
+      {
+        title: 'Services',
+        isShow: false,
+      },
+      {
+        title: 'Account',
+        isShow: false,
+      },
+      {
+        title: 'Apple Store',
+        isShow: false,
+      },
+      {
+        title: 'For Business',
+        isShow: false,
+      },
+      {
+        title: 'For Education',
+        isShow: false,
+      },
+      {
+        title: 'For Healthcare',
+        isShow: false,
+      },
+      {
+        title: 'For Government',
+        isShow: false,
+      },
+      {
+        title: 'Apple Values',
+        isShow: false,
+      },
+      {
+        title: 'About Apple',
+        isShow: false,
+      },
+    ],
+  }),
+}
+</script>
+
+<style lang="scss">
+@import '~assets/variables';
+.div_container {
+  font-family: 'SF Pro';
+  font-size: 12px;
+  background-color: #f5f5f7;
+
+  //   Foot Notes
+  .foot_notes_container {
+    padding: 17px 22px 11px;
+    max-width: 980px;
+    margin: 0 auto;
+    .foot_notes {
+      color: #6e6e73;
+      line-height: 1.33337;
+      font-weight: 400;
+      letter-spacing: -0.01em;
+      padding-bottom: 0.8em;
+    }
+    border-bottom: 1px solid $footer-border-color;
+  }
+
+  //   Breadcrumbs
+  .breadcrumbs_container {
+    max-width: 980px;
+    padding: 17px 22px;
+    margin: 0 auto;
+    color: #424245;
+    .breadcrumbs {
+      display: flex;
+      justify-content: space-between;
+      max-width: 200px;
+      height: 15px;
+    }
+  }
+
+  //   Apple Directory
+  .apple_directory_container {
+    max-width: 980px;
+    padding: 0 22px;
+    margin: 0 auto;
+    color: #424245;
+    div {
+      justify-content: space-between;
+      div {
+        margin-bottom: 0.8em;
+        cursor: pointer;
+        .apple_title {
+          color: #1d1d1f;
+          font-weight: 600;
+          cursor: default;
+        }
+      }
+    }
+    .section_title {
+      border-bottom: 1px solid #d2d2d7;
+      padding: 10px 0;
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  //   Footer
+  .footer_container {
+    color: #6e6e73;
+    padding: 34px 22px;
+    max-width: 980px;
+    margin: 0 auto;
+    .footer_shop {
+      margin-bottom: 7px;
+      padding-bottom: 8px;
+      border-bottom: 1px solid #d2d2d7;
+      .link {
+        color: #0066cc;
+      }
+    }
+    .footer_legal {
+      display: flex;
+      flex-wrap: wrap;
+      line-height: 20px;
+      .copyright {
+        padding-right: 0.7rem;
+      }
+      .legal_link {
+        padding-right: 0.7rem;
+        margin-right: 0.7rem;
+      }
+      .border_r {
+        border-right: 1px solid #d2d2d7;
+      }
+    }
+  }
+}
+</style>
